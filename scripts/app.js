c04c01b4048fbd15170850acac4cc0dcae71153b
Karly <Karly@hungryhoffkitchen.attlocal.net>
2016-12-17 13:25:43 -0600

    Added save, delete and new task functionality to list.

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,29 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope, dataService){
-	
+	$scope.addTodo = function(){
+		var todo = {task: "New Task"};
+		$scope.todos.push(todo);
+	};
 	dataService.getTodos(function(res) {
 		$scope.todos = res.data;
 	});
-
+	$scope.deleteTodo = function(todo, $index){
+		dataService.deleteTodo(todo);
+		$scope.todos.splice($index, 1);
+	};
+	$scope.saveTodo = function(todo){
+		dataService.saveTodo(todo);
+	};
 })
 .service('dataService', function($http) {
 	this.getTodos = function(callback) {
 		$http.get('../data/todos.json')
 		.then(callback)
-	}
+	};
+	this.deleteTodo = function(todo){
+		console.log('"' + todo.task + '"' + " task was deleted.")
+	};
+	this.saveTodo = function(todo){
+		console.log('"' + todo.task + '"' + " task was saved.")
+	};
 });
